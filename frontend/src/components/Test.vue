4c5ba3e0eb028aa71a657367b25cb6936e3e0eb0
JK <jarekkornowicz@googlemail.com>
2021-04-10 00:20:28 +0200

    vue and positions

---
+++ frontend/src/components/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="test">
+    <ul>
+      <li v-for="puzzle in puzzles" :key="puzzle.id">{{ puzzle.id }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import puzzles from "../data/puzzles_alg.json";
+
+export default {
+  name: "Test",
+  data() {
+    return {
+      puzzles,
+    };
+  },
+};
+</script>
+
+<style></style>
