dee43edc9d35a4c53a9eebe7280ad566af9376ae
JK <jarekkornowicz@googlemail.com>
2021-04-11 19:38:04 +0200

    dummy data and cleaning state

--- frontend/src/store/puzzles/index.js
+++ frontend/src/store/puzzles/index.js
@@ -2,13 +2,31 @@ export default {
   namespaced: true,
   state: {
     puzzles: [],
-    activePuzzle: {},
+    activePuzzleExists: false,
+    activePuzzle: {
+      moves: 'a8a6 h7h6 e6d5 h6a6',
+      rating: 1080,
+      themes: 'crushing endgame short skewer',
+      gameurl: 'https://lichess.org/N9FWAV9a/black#110',
+      moves_alg: ['Rxa6', 'Rh6+', 'Kd5', 'Rxa6'],
+      fen_after_first_move: '8/7R/r3k3/4p2p/3b2p1/3K4/8/5R2 w - - 0 56',
+      placement: '8/7R/r3k3/4p2p/3b2p1/3K4/8/5R2',
+      active: 'w',
+      castling: '-',
+      enpassant: '-',
+      number_of_pieces: 9,
+      positions: {
+        K: ['d3'],
+        R: ['f1', 'h7'],
+        k: ['e6'],
+        r: ['a6'],
+        b: ['d4'],
+        p: ['e5', 'g4', 'h5'],
+      },
+      id: 169,
+    },
     puzzleHistory: {},
-    ratingMin: 400,
-    ratingMax: 3000,
     ratingRange: [800, 1500],
-    numberOfPiecesMin: 3,
-    numberOfPiecesMax: 32,
     numberOfPiecesRange: [4, 7],
   },
   mutations: {
@@ -22,6 +40,7 @@ export default {
       state.numberOfPiecesRange = payload;
     },
     updateActivePuzzle(state, payload) {
+      state.activePuzzleExists = true;
       state.activePuzzle = payload;
     },
   },
