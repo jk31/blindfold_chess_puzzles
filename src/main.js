eb9dad18b2126dc9f062f75b46625f0bbc8a8f4c
JK <jarekkornowicz@googlemail.com>
2021-04-13 00:26:01 +0200

    removed fontweseome imports

--- src/main.js
+++ src/main.js
@@ -6,13 +6,6 @@ import store from './store';
 
 import vuetify from './plugins/vuetify';
 
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
-
-Vue.component('font-awesome-icon', FontAwesomeIcon);
-library.add(fas);
-
 Vue.config.productionTip = false;
 
 new Vue({
